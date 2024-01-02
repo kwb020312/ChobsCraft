ebde53462fea9ef047e25daae3808c80ac9e9d58
김우빈 <kwb020312@naver.com>
2024-01-02 23:51:50 +0900

    Feat: 하늘 및 태양광 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Canvas } from '@react-three/fiber';
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
     <>
       <div>Outside Canvas</div>
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
       </Canvas>
     </>
   );
