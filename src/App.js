bd91f31eaddb7c527d103f42b7488e5bed82dd22
김우빈 <kwb020312@naver.com>
2024-01-04 22:09:48 +0900

    Feat: 마우스 카메라 뷰

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Ground from "./components/Ground";
 import Player from "./components/Player";
+import FPV from "./components/FPV";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
+        <FPV />
         <Physics>
           <Player />
           <Ground />
