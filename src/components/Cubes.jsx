959c1d23bc0dc8cd288e849333a6ef137915560f
김우빈 <kwb020312@naver.com>
2024-02-02 22:57:57 +0900

    Feat: Mouse Hover 이벤트 추가

--- src/components/Cubes.jsx
+++ src/components/Cubes.jsx
@@ -1,8 +1,10 @@
 import { useBox } from "@react-three/cannon";
 import { useStore } from "../hooks/useStore";
 import * as textures from "../images/textures";
+import { useState } from "react";
 
 const Cube = ({ position, texture }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const [ref] = useBox(() => ({
     type: "Static",
     position,
@@ -16,6 +18,14 @@ const Cube = ({ position, texture }) => {
 
   return (
     <mesh
+      onPointerMove={(e) => {
+        e.stopPropagation();
+        setIsHovered(true);
+      }}
+      onPointerOut={(e) => {
+        e.stopPropagation();
+        setIsHovered(false);
+      }}
       onClick={(e) => {
         e.stopPropagation();
         const clickedFace = Math.floor(e.faceIndex / 2);
@@ -31,7 +41,13 @@ const Cube = ({ position, texture }) => {
       ref={ref}
     >
       <boxBufferGeometry attach="geometry" />
-      <meshStandardMaterial map={activeTexture} attach="material" />
+      <meshStandardMaterial
+        color={isHovered ? "gray" : "white"}
+        map={activeTexture}
+        attach="material"
+        transparent
+        opacity={texture === "glass" ? 0.6 : 1}
+      />
     </mesh>
   );
 };
